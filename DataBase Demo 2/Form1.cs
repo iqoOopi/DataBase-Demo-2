245325073dfa74532eb34b9713539cfc34dde1fb
iqoOopi <46897552+iqoOopi@users.noreply.github.com>
2019-03-08 13:44:24 -0700

    init

--- DataBase Demo 2/Form1.cs
+++ DataBase Demo 2/Form1.cs
@@ -16,5 +16,20 @@ namespace DataBase_Demo_2
         {
             InitializeComponent();
         }
+
+        private void productsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.mMABooksDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'mMABooksDataSet.Products' table. You can move, or remove it, as needed.
+            this.productsTableAdapter.Fill(this.mMABooksDataSet.Products);
+
+        }
     }
 }
